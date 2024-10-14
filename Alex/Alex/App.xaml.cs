e70caed57ec7295fc8a8bdc6f266ec33e8843b53
Mafessita <maria.garciaco@cecar.edu.co>
2024-10-14 18:29:07 -0500

    Proyecto

---
+++ Alex/Alex/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Alex
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
